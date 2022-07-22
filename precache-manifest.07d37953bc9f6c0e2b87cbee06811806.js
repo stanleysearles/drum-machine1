e7ab3acb73d12cd3f0c50b4e9e442dcbcaf286cc
Matthias Senkowski <stanleysearles3@gmail.com>
2022-07-22 16:54:34 +0200

    Updates

---
+++ precache-manifest.07d37953bc9f6c0e2b87cbee06811806.js
@@ -0,0 +1,58 @@
+self.__precacheManifest = [
+  {
+    "revision": "e99b30eb668f821704eb",
+    "url": "/drum-machine1/static/js/main.e99b30eb.chunk.js"
+  },
+  {
+    "revision": "f32e39cb70066c7c661f",
+    "url": "/drum-machine1/static/js/1.f32e39cb.chunk.js"
+  },
+  {
+    "revision": "3fd23801872bff25dda7",
+    "url": "/drum-machine1/static/js/2.3fd23801.chunk.js"
+  },
+  {
+    "revision": "fed1de7993b91bda25a7",
+    "url": "/drum-machine1/static/js/runtime~main.fed1de79.js"
+  },
+  {
+    "revision": "20f8cf56ac1c63638f25b76321febd5a",
+    "url": "/drum-machine1/static/media/kick1.20f8cf56.mp3"
+  },
+  {
+    "revision": "9e638ef2bbe5bbbb8a5da35b3f25aef9",
+    "url": "/drum-machine1/static/media/kick2.9e638ef2.mp3"
+  },
+  {
+    "revision": "3827ce42a683ed964d36ce09874c2e8e",
+    "url": "/drum-machine1/static/media/openhihat.3827ce42.mp3"
+  },
+  {
+    "revision": "146b34794980f154b906bb56deaaae4f",
+    "url": "/drum-machine1/static/media/closedhihat.146b3479.mp3"
+  },
+  {
+    "revision": "04e3ca35faa8bd0e72abb441c6537a23",
+    "url": "/drum-machine1/static/media/snare.04e3ca35.mp3"
+  },
+  {
+    "revision": "b75d84290a6b5ffb2adb5723a77580fa",
+    "url": "/drum-machine1/static/media/rim.b75d8429.mp3"
+  },
+  {
+    "revision": "6146f8e61e1adf863e2e4c68733e623b",
+    "url": "/drum-machine1/static/media/shaker.6146f8e6.mp3"
+  },
+  {
+    "revision": "ebc3a64f42d459d71eb13e33c018154d",
+    "url": "/drum-machine1/static/media/cowbell.ebc3a64f.mp3"
+  },
+  {
+    "revision": "69d14df4ea6e92c0f3557b08b92151c0",
+    "url": "/drum-machine1/static/media/conga.69d14df4.mp3"
+  },
+  {
+    "revision": "323bc883908a5fe0cf766199415d5f41",
+    "url": "/drum-machine1/index.html"
+  }
+];
